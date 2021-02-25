b558dacae909cdcc7ffac5dd968bbe1610fb48ed
ttran92 <v9tran@gmail.com>
2021-02-25 10:31:01 -0500

    button changes color when you hover over it

---
+++ script.js
@@ -0,0 +1,5 @@
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector('#pause');
+const resetButton = document.querySelector('#reset');
+const theInterval = document.querySelector(".timer");
+
